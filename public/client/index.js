6d46fe37bdcbb5a92dda3e5ec4fb1d9364546f14
Chi Kim <chi@kim.club>
2022-02-11 17:27:25 -0800

    adds svg graph

--- public/client/index.js
+++ public/client/index.js
@@ -2,49 +2,17 @@ import { generate, BINARY_TREE } from "./lib/generate.js";
 import { bfs } from "./lib/traversal.js";
 import { drawSVG } from "./lib/svg.js";
 
-const MIN_BOX_WIDTH = 10;
-
 const nodeNumInput$ = document.querySelector("#nodeNumInput");
 const nodeNumButton$ = document.querySelector("#nodeNumButton");
 const generatedGraphStr$ = document.querySelector("#generatedGraphStr");
-const gridContainer$ = document.querySelector("#gridContainer");
-const [gridParent$, svgContainer$] = document.querySelectorAll(".gridParent");
+const svgContainer$ = document.querySelector(".gridParent");
 
 let graph = null;
 
-function Box(boxWidth) {
-  const div = document.createElement("div");
-  div.classList.add("box");
-  div.style.width = `${boxWidth}px`;
-  div.style.height = `${boxWidth}px`;
-  gridContainer$.append(div);
-
-  this.paint = () => {
-    div.style.backgroundColor = "#afa";
-  };
-}
-
-const clearGrid = () => {
-  gridContainer$.innerHTML = "";
-  grid = [];
-};
-
-let grid = [];
-const drawGrid = (width, height, boxWidth) => {
-  if (!height) return;
-  const row = new Array(width).fill(0).map((_) => {
-    return new Box(boxWidth);
-  });
-  grid.push(row);
-  return drawGrid(width, height - 1, boxWidth);
-};
-
-const setGraphMetaData = () => {
-  let graphHeight = 0;
+const draw = () => {
   let levels = [];
   bfs(graph, (n, h) => {
     n.heightIndex = h - 1;
-    graphHeight = Math.max(graphHeight, h);
 
     const row = levels[h - 1] || [];
     if (n.left) {
@@ -60,58 +28,6 @@ const setGraphMetaData = () => {
   });
 
   drawSVG(svgContainer$, levels);
-
-  const maxWidth = Math.pow(2, graphHeight);
-
-  let leftMost = maxWidth;
-  let rightMost = 0;
-
-  const setColumn = (start, end, root) => {
-    if (!root) {
-      return;
-    }
-    const row = grid[root.heightIndex];
-    const col = Math.ceil((start + end) / 2);
-
-    root.leafIndex = col;
-    leftMost = Math.min(leftMost, col);
-    rightMost = Math.max(rightMost, col);
-
-    // don't want end to be smaller than start
-    setColumn(start, Math.max(start, col - 1), root.left);
-
-    setColumn(Math.min(col + 1, end), end, root.right);
-  };
-
-  setColumn(0, maxWidth, graph);
-
-  bfs(graph, (n, h) => {
-    n.leafIndex -= leftMost;
-  });
-
-  return {
-    maxWidth: rightMost - leftMost + 1,
-    graphHeight,
-  };
-};
-
-const draw = () => {
-  const { maxWidth, graphHeight } = setGraphMetaData();
-
-  const boxWidth = Math.max(
-    Math.min(gridParent$.clientWidth / maxWidth),
-    MIN_BOX_WIDTH
-  );
-
-  gridContainer$.style.width = `${maxWidth * boxWidth}px`;
-  gridContainer$.style.height = `${graphHeight * boxWidth}px`;
-
-  clearGrid();
-  drawGrid(maxWidth, graphHeight, boxWidth);
-
-  bfs(graph, (n, h) => {
-    grid[n.heightIndex][n.leafIndex].paint();
-  });
 };
 
 nodeNumButton$.addEventListener("click", () => {
---
+++ public/client/lib/svg.js
@@ -0,0 +1,101 @@
+const SPACE = 50;
+const CHILD_SKEW = 20;
+const BOX_SIZE = 60;
+
+function SvgNode(parent$, node, hSpace) {
+  const element = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+
+  const radius = BOX_SIZE / 2;
+
+  let x = node.columnIndex * (SPACE + BOX_SIZE) + radius;
+  if (hSpace) {
+    x = (node.columnIndex + 1) * hSpace + node.columnIndex * BOX_SIZE + radius;
+  }
+
+  x += CHILD_SKEW;
+
+  const y = node.rowIndex * (SPACE + BOX_SIZE) + radius;
+
+  if (node.parent) {
+    if (node.parent.svgNode.xPos === x) {
+      if (node.isLeftChild) {
+        x -= CHILD_SKEW;
+      }
+      if (node.isRightChild) {
+        x += CHILD_SKEW;
+      }
+    }
+    node.parent.svgNode.drawLine(x, y);
+  }
+
+  element.setAttributeNS(null, "cx", x);
+  element.setAttributeNS(null, "cy", y);
+  element.setAttributeNS(null, "r", radius);
+  element.setAttributeNS(null, "fill", "yellow");
+  // used for debugging. This way we could log the node and see what svg it corresponds to in the inspector
+  this.svgElement = element;
+
+  this.xPos = x;
+  this.yPos = y;
+
+  parent$.appendChild(element);
+
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  text.setAttributeNS(null, "x", x);
+  text.setAttributeNS(null, "y", y);
+  text.setAttributeNS(null, "font-size", 20);
+  text.setAttributeNS(null, "text-anchor", "middle");
+  text.setAttributeNS(null, "fill", "black");
+  text.textContent = node.value;
+  parent$.appendChild(text);
+
+  this.drawLine = (fromX, fromY) => {
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+    line.setAttributeNS(null, "x1", x);
+    line.setAttributeNS(null, "x2", fromX);
+    line.setAttributeNS(null, "y1", y);
+    line.setAttributeNS(null, "y2", fromY);
+    line.setAttributeNS(
+      null,
+      "style",
+      "stroke:rgb(200, 255,200);stroke-width:2"
+    );
+
+    parent$.appendChild(line);
+  };
+}
+
+export const drawSVG = (container$, levels) => {
+  const widest = levels.reduce((acc, row) => {
+    return Math.max(row.length, acc);
+  }, 0);
+
+  const width = widest * BOX_SIZE + (widest - 1) * SPACE;
+  const height = levels.length * BOX_SIZE + (levels.length - 1) * SPACE;
+
+  container$.innerHTML = `
+  <svg version="1.1" id="svgContainer"
+     width="${width + 2 * CHILD_SKEW}" height="${height}" 
+     xmlns="http://www.w3.org/2000/svg">
+    </svg>
+  `;
+
+  const svgContainer$ = document.querySelector("#svgContainer");
+
+  levels.forEach((row, rowIndex) => {
+    row.forEach((node, columnIndex) => {
+      node.rowIndex = rowIndex;
+      node.columnIndex = columnIndex;
+
+      const horizontalSpace =
+        row.length < widest
+          ? Math.floor((width - BOX_SIZE * row.length) / (row.length + 1))
+          : 0;
+      node.svgNode = new SvgNode(svgContainer$, node, horizontalSpace);
+    });
+  });
+};
